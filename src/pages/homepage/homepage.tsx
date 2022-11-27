de856cfe3d6f726681eeb69fbe487180eb55390c
Wagner Guimarães <wagnerluizsg@outlook.com.br>
2022-11-27 11:22:53 -0300

    iniciando homepage

--- src/pages/homepage/homepage.tsx
+++ src/pages/homepage/homepage.tsx
@@ -1,8 +1,36 @@
 
 const Homepage = () => {
     return (
-        <div>
-            <h1>HOMEPAGE</h1>
+
+
+        <div className="homepage-container">
+
+
+            <section className="homepage-slogan">
+
+                <div className="homepage-content">
+                    <h1>
+                        <span>Best</span> {" "}
+                        way to
+                    </h1>
+                    <h1>manage <span>your</span></h1>
+                    <h1>finance</h1>
+
+                    <h3>With this platform you can manage your all</h3>
+                    <h3> finance very easily & fast.</h3>
+
+                </div>
+
+
+
+
+            </section>
+
+
+
+            <aside className="homepage-images">
+
+            </aside>
         </div>
     )
 }
