631e8e260cdaae95486b321ad3cfdfada660d2bd
Wagner Guimarães <wagnerluizsg@outlook.com.br>
2022-11-27 16:04:34 -0300

    alterando responsivo da homepage

--- src/pages/homepage/homepage.tsx
+++ src/pages/homepage/homepage.tsx
@@ -6,6 +6,7 @@ const Homepage = () => {
         <div className="homepage-container">
 
             <section className="homepage-l">
+
                 <aside className="homepage-text">
                     <h1>Best <span>way to</span></h1>
                     <h1>manage <span>your</span></h1>
@@ -13,6 +14,8 @@ const Homepage = () => {
                     <p>with this plataform you can manage your all finance easily & fast.</p>
                     <button id="button">Get Started</button>
                 </aside>
+
+                
             </section>
             <section className="homepage-r">
             </section>
