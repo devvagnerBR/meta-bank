80b5bb3743750eedba7a55147d1cb86b51198eaf
Wagner Guimarães <wagnerluizsg@outlook.com.br>
2022-11-30 09:17:56 -0300

    iniciando contrução do signup form

--- src/pages/signup/signup.tsx
+++ src/pages/signup/signup.tsx
@@ -3,7 +3,8 @@ import React from 'react'
 const Signup = () => {
 
     return (
-        <div className='signup-container'>
+        
+        <div className='signup-container animeLeft'>
             <header className='signup-header'>
                 <h1>Login</h1>
                 <h3>Bem vindo de volta!</h3>
