c18a9448d0059187c384234cca5d9764413a0b7d
Wagner Guimarães <wagnerluizsg@outlook.com.br>
2022-12-04 10:13:50 -0300

    navbar finalizada

--- src/pages/private/history/history.tsx
+++ src/pages/private/history/history.tsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import Navbar from '../../../components/navBar/navBar'
 
 const History = () => {
+
+
     return (
-        <div>History</div>
+        <div className='history-container'>
+            <header className='header-history-container'>
+                <h1>Wagner</h1>
+                <h4>Extrato</h4>
+            </header>
+            <section className='history-content-container'>
+
+            </section>
+
+            <Navbar />
+        </div>
     )
 }
 
--- src/pages/private/transaction/transaction.tsx
+++ src/pages/private/transaction/transaction.tsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import Navbar from '../../../components/navBar/navBar'
 
 const Transaction = () => {
     return (
-        <div>Transaction</div>
+        <div className='transaction-container'>
+            <header className='header-transaction-container'>
+                <h1>Wagner</h1>
+                <h4>Transferir</h4>
+            </header>
+            <section className='transaction-content-container'>
+            </section>
+            <Navbar />
+        </div>
     )
 }
 
